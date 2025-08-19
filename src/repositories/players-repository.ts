6ca7dc2cbdca89aa2d1a4f27c634ef195ca7d663
ikarosuu <icaro.code@proton.me>
2025-08-19 14:01:03 -0400

    get list all players

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,9 +1,88 @@
 import { PlayerModel } from "../models/player-model"
 
 const database: PlayerModel[] = [
-    { id: 1, name: "Messi" },
-    { id: 2, name: "Ronaldo" },
-]
+  {
+    id: 1,
+    name: "Lionel Messi",
+    club: "Paris Saint-Germain",
+    nationality: "Argentina",
+    position: "Forward",
+    statistics: {
+      Overall: 93,
+      Pace: 85,
+      Shooting: 94,
+      Passing: 91,
+      Dribbling: 95,
+      Defending: 38,
+      Physical: 65,
+    },
+  },
+  {
+    id: 2,
+    name: "Cristiano Ronaldo",
+    club: "Manchester United",
+    nationality: "Portugal",
+    position: "Forward",
+    statistics: {
+      Overall: 91,
+      Pace: 87,
+      Shooting: 93,
+      Passing: 82,
+      Dribbling: 88,
+      Defending: 35,
+      Physical: 77,
+    },
+  },
+  {
+    id: 3,
+    name: "Kevin De Bruyne",
+    club: "Manchester City",
+    nationality: "Belgium",
+    position: "Midfielder",
+    statistics: {
+      Overall: 91,
+      Pace: 76,
+      Shooting: 86,
+      Passing: 93,
+      Dribbling: 88,
+      Defending: 64,
+      Physical: 78,
+    },
+  },
+  {
+    id: 4,
+    name: "Robert Lewandowski",
+    club: "Bayern Munich",
+    nationality: "Poland",
+    position: "Forward",
+    statistics: {
+      Overall: 92,
+      Pace: 78,
+      Shooting: 92,
+      Passing: 79,
+      Dribbling: 86,
+      Defending: 44,
+      Physical: 82,
+    },
+  },
+  {
+    id: 5,
+    name: "Neymar Jr",
+    club: "Paris Saint-Germain",
+    nationality: "Brazil",
+    position: "Forward",
+    statistics: {
+      Overall: 91,
+      Pace: 91,
+      Shooting: 85,
+      Passing: 86,
+      Dribbling: 94,
+      Defending: 37,
+      Physical: 62,
+    },
+  },
+];
+
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
     return database
