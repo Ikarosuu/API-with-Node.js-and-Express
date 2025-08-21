9f96bb3834bd678d0322cde44a6bc35a4fd3f737
ikarosuu <icaro.code@proton.me>
2025-08-21 16:21:15 -0400

    created delete service and repository

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -43,5 +43,10 @@ export const createPlayerService = async (player: PlayerModel) => {
 
 export const deletePlayerService = async (id: number) => {
     let response = null
-    
+
+    await PlayerRepository.deleteOnePlayer(id)
+
+    response = HttpResponse.ok({message: "deleted"})
+    return response
+
 }
