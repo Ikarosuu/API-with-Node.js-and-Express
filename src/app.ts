ee08893545c711c50e9bc23eb1051cd64d2f2e6a
ikarosuu <icaro.code@proton.me>
2025-08-22 14:47:09 -0400

    read a json file and cors implementation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { json } from "express"
 import router from "./routes"
+import cors from "cors"
 
 function createApp(){
     const app = express()
 
     app.use(json())
     app.use("/api", router)
+    app.use(cors())
+
+    const corsOptions = {
+        origin: ["https://www.exemplo.com/", "http://www.exemplo2.com"],
+        methods: ["GET"],
+    }
+
+    app.use(cors(corsOptions))
 
     return app
 }
